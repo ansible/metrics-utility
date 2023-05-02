1b95f39a06c76cd569944fd9e03f7b64563b5237
Martin Slemr <mslemr@redhat.com>
2023-05-02 19:07:16 +0200

    AWX_PATH env

--- metrics_utility/__init__.py
+++ metrics_utility/__init__.py
@@ -1,17 +1,23 @@
 import os
 import sys
+import importlib.util
 
 from metrics_utility.management_utility import ManagementUtility
-
-os.environ.setdefault('AWX_DIR', '/awx_devel')
-
-sys.path.append("/awx_devel")
-from awx import prepare_env
-import django
 
 
 def manage():
     """Run a ManagementUtility."""
+    awx_path = os.getenv('AWX_PATH', '/awx_devel')
+    sys.path.append(awx_path)
+
+    spec = importlib.util.find_spec('awx')
+    if spec is None:
+        sys.stderr.write(f"Automation Controller modules not found in {awx_path}\n")
+        exit(-1)
+
+    from awx import prepare_env
+    import django
+
     prepare_env()
     django.setup()
 
