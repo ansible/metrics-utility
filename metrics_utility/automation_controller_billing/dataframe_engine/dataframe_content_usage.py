bcd8456d9590eb2e1b7a136dc12c146c7797ab41
Ladislav Smola <lsmola@redhat.com>
2024-05-15 14:45:31 +0200

    Cleanup dataframe engines

--- metrics_utility/automation_controller_billing/dataframe_engine/dataframe_content_usage.py
+++ metrics_utility/automation_controller_billing/dataframe_engine/dataframe_content_usage.py
@@ -4,73 +4,26 @@ import pandas as pd
 import re
 from dateutil.relativedelta import relativedelta
 
+from metrics_utility.automation_controller_billing.dataframe_engine.base \
+    import Base, list_dates, granularity_cast
+
 logger = logging.getLogger(__name__)
 
 #######################################
-# Code for build the dataframe report and pushing it back to S3
+# Code for build the dataframe report
 ######################################
 
-def granularity_cast(date, granularity):
-    if granularity == "monthly":
-        return date.replace(day=1)
-    elif granularity == "yearly":
-        return date.replace(month=1, day=1)
-    else:
-        return date
 
-
-def list_dates(start_date, end_date, granularity):
-    # Given start date and end date, return list of dates in the given granularity
-    # e.g. for daily it is a list of days withing the interval, for monthly it is a
-    # list of months withing the interval, etc.
-    start_date = granularity_cast(start_date, granularity)
-    end_date = granularity_cast(end_date, granularity)
-
-    dates_arr = []
-    while start_date < end_date:
-        dates_arr.append(start_date)
-
-        if granularity == "monthly":
-            start_date += relativedelta(months=+1)
-        elif granularity == "yearly":
-            start_date += relativedelta(years=+1)
-        else:
-            start_date += datetime.timedelta(days=1)
-
-    dates_arr.append(end_date)
-
-    return dates_arr
-
-class DataframeContentUsage():
+class DataframeContentUsage(Base):
     LOG_PREFIX = "[AAPBillingReport] "
 
-    def __init__(self, extractor, month, extra_params):
-        self.logger = logger
-
-        self.extractor = extractor
-        self.month = month
-        self.extra_params = extra_params
-
-        self.price_per_node = extra_params['price_per_node']
-
-    @staticmethod
-    def get_logger():
-        return logging.getLogger(__name__)
-
-
     def build_dataframe(self):
-        # A daily rollup dataframe
-        billing_data_monthly_rollup = None
-
         # Get list of days of the specified month for the monthly report
         beginning_of_the_month = self.month.replace(day=1)
         end_of_the_month = beginning_of_the_month + relativedelta(months=1) - relativedelta(days=1)
         dates_list = list_dates(start_date=beginning_of_the_month,
                                 end_date=end_of_the_month,
                                 granularity="daily")
-
-        granularity = "daily"
-        table_name = "job_events_fk"
 
         # A monthly rollup dataframe
         content_explorer_rollup = None
@@ -111,8 +64,6 @@ class DataframeContentUsage():
 
                 events['collection_name'] = events['module_name'].apply(
                     self.extract_collection_name)
-                # events['role_collection_name'] = events['role_name'].apply(
-                #     self.extract_collection_name)
 
                 # Final cleanup if some module names didn't connect, otherwise this will fail
                 # to insert with not null constraint on module_name
@@ -163,25 +114,9 @@ class DataframeContentUsage():
         ccsp_report = content_explorer_rollup.reset_index()
 
         ccsp_report = ccsp_report.reindex(columns=[
-            'host_name', 'module_name', 'collection_name', 'role_name', 'install_uuid', 'job_remote_id', 'task_runs', 'duration', ])
+            'host_name', 'module_name', 'collection_name', 'role_name', 'install_uuid',
+            'job_remote_id', 'task_runs', 'duration', ])
         return ccsp_report
-
-    def cast_dataframe(self, df, types):
-        levels = []
-        for index, level in enumerate(df.index.levels):
-            casted_level = df.index.levels[index].astype(object)
-            levels.append(casted_level)
-
-        df.index = df.index.set_levels(levels)
-
-        return df.astype(types)
-
-    def summarize_merged_dataframes(self, df, columns):
-        for col in columns:
-            df[col] = df[[f"{col}_x", f"{col}_y"]].sum(axis=1)
-            del df[f"{col}_x"]
-            del df[f"{col}_y"]
-        return df
 
     @staticmethod
     def collection_regexp():
--- metrics_utility/automation_controller_billing/dataframe_engine/dataframe_jobhost_summary_usage.py
+++ metrics_utility/automation_controller_billing/dataframe_engine/dataframe_jobhost_summary_usage.py
@@ -3,57 +3,17 @@ import datetime
 import pandas as pd
 from dateutil.relativedelta import relativedelta
 
+from metrics_utility.automation_controller_billing.dataframe_engine.base \
+    import Base, list_dates
+
 logger = logging.getLogger(__name__)
 
 #######################################
-# Code for build the dataframe report and pushing it back to S3
+# Code for build the dataframe report
 ######################################
 
-def granularity_cast(date, granularity):
-    if granularity == "monthly":
-        return date.replace(day=1)
-    elif granularity == "yearly":
-        return date.replace(month=1, day=1)
-    else:
-        return date
-
-
-def list_dates(start_date, end_date, granularity):
-    # Given start date and end date, return list of dates in the given granularity
-    # e.g. for daily it is a list of days withing the interval, for monthly it is a
-    # list of months withing the interval, etc.
-    start_date = granularity_cast(start_date, granularity)
-    end_date = granularity_cast(end_date, granularity)
-
-    dates_arr = []
-    while start_date < end_date:
-        dates_arr.append(start_date)
-
-        if granularity == "monthly":
-            start_date += relativedelta(months=+1)
-        elif granularity == "yearly":
-            start_date += relativedelta(years=+1)
-        else:
-            start_date += datetime.timedelta(days=1)
-
-    dates_arr.append(end_date)
-
-    return dates_arr
-
-class DataframeJobhostSummaryUsage():
+class DataframeJobhostSummaryUsage(Base):
     LOG_PREFIX = "[AAPBillingReport] "
-
-    def __init__(self, extractor, month, extra_params):
-        self.logger = logger
-
-        self.extractor = extractor
-        self.month = month
-        self.extra_params = extra_params
-
-    @staticmethod
-    def get_logger():
-        return logging.getLogger(__name__)
-
 
     def build_dataframe(self):
         # A daily rollup dataframe
@@ -130,23 +90,6 @@ class DataframeJobhostSummaryUsage():
             return None
 
         return billing_data_monthly_rollup.reset_index()
-
-    def cast_dataframe(self, df, types):
-        levels = []
-        for index, level in enumerate(df.index.levels):
-            casted_level = df.index.levels[index].astype(object)
-            levels.append(casted_level)
-
-        df.index = df.index.set_levels(levels)
-
-        return df.astype(types)
-
-    def summarize_merged_dataframes(self, df, columns):
-        for col in columns:
-            df[col] = df[[f"{col}_x", f"{col}_y"]].sum(axis=1)
-            del df[f"{col}_x"]
-            del df[f"{col}_y"]
-        return df
 
     @staticmethod
     def unique_index_columns():
