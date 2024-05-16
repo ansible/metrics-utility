59640b6692275a3b4aa1f063ad1ab22d732822e6
Ladislav Smola <lsmola@redhat.com>
2024-05-16 17:11:55 +0200

    Bump the utility version

--- metrics_utility/automation_controller_billing/collectors.py
+++ metrics_utility/automation_controller_billing/collectors.py
@@ -114,7 +114,7 @@ def config(since, **kwargs):
         'logging_aggregators': settings.LOG_AGGREGATOR_LOGGERS,
         'external_logger_enabled': settings.LOG_AGGREGATOR_ENABLED,
         'external_logger_type': getattr(settings, 'LOG_AGGREGATOR_TYPE', None),
-        'metrics_utility_version': "0.2.0", # TODO read from setup.cfg
+        'metrics_utility_version': "0.3.0", # TODO read from setup.cfg
         'billing_provider_params': {} # Is being overwritten in collector.gather by set ENV VARS
     }
 
