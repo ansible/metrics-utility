bf0aca5c02b7fd6188c97a5cb37bfb5966c47285
MilanPospisil <arkanus@seznam.cz>
2025-01-31 12:32:10 +0100

    Working comparing existing reports with different params

--- metrics_utility/test/snapshot_tests/CCSP/CCSP_snapshot_test.py
+++ metrics_utility/test/snapshot_tests/CCSP/CCSP_snapshot_test.py
@@ -5,16 +5,16 @@ import pytest
 def test_snapshot():    
     snapshot_utils.run_and_test_snapshot_definitions('./metrics_utility/test/snapshot_tests/CCSP/data/')
 
-    return
-
+    print('/nNow comparing original reports pairs that should hold the same result:/n')
     # compare test with different params that should hold the same result (except ignored fields)
     prefix = './metrics_utility/test/snapshot_tests/CCSP/data/CCSPv2/'
-    report1 = prefix + 'snapshot_def_2024-02-01--2024-02-29/report.xlsx'
-    report2 = prefix + 'snapshot_def_2024-02/report.xlsx'
+    report1 = prefix + 'special_snapshot_def_2024-02-01--2024-02-29/report.xlsx'
+    report2 = prefix + 'special_snapshot_def_2024-02/report.xlsx'
     snapshot_utils.compare_CCSPv2_reports(report1, report2)
 
-    report1 = prefix + 'snapshot_def_2024-03-01--2024-03-31/report.xlsx'
-    report2 = prefix + 'snapshot_def_2024-03/report.xlsx'
+
+    report1 = prefix + 'special_snapshot_def_2024-03-01--2024-03-31/report.xlsx'
+    report2 = prefix + 'special_snapshot_def_2024-03/report.xlsx'
     snapshot_utils.compare_CCSPv2_reports(report1, report2)
 
     snapshot_utils.compare_CCSPv2_reports(report1, report2)
