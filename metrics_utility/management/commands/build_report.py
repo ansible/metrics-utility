183a4d617880845f5bfd757dbe2869ee7923679d
MilanPospisil <arkanus@seznam.cz>
2025-02-06 16:48:25 +0100

    Simplify error message

--- metrics_utility/management/commands/build_report.py
+++ metrics_utility/management/commands/build_report.py
@@ -124,18 +124,12 @@ class Command(BaseCommand):
             extra_params=extra_params).create()
 
         if report_dataframe[0] is None or report_dataframe[0].empty:
-            if (opt_since is not None or opt_until is not None):
-                log = 'No billing data for date range: '
-                if (opt_since is not None):
-                    log += f' since={opt_since}'
-                if (opt_until is not None):
-                    log += f' until={opt_until}'
-                self.logger.info(log)
-            elif (opt_month is not None):
-                self.logger.info(f"No billing data for month: {opt_month}")
+            if opt_since is not None:
+                self.logger.info(f'No billing data for input date range {extra_params["opt_since"]}--{extra_params["opt_until"]}')
             else:
-                self.logger.info('No billing data for input date range')
+                self.logger.info(f'No billing data for month {opt_month}')
             return
+
 
         report_engine = ReportFactory(report_period=opt_month,
                                       report_dataframe=report_dataframe,
