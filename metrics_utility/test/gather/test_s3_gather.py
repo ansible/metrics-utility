ede622f87c47891b1d87db515b094447360dbc80
Martin Hradil <mhradil@redhat.com>
2025-03-06 14:35:15 +0000

    s3 test: make sure upload succeeds
    Issue: AAP-38819
    Issue: AAP-39221
    Issue: AAP-41884

---
+++ metrics_utility/test/gather/test_s3_gather.py
@@ -0,0 +1,34 @@
+import os
+import pytest
+import subprocess
+import sys
+
+env_vars = {
+    'METRICS_UTILITY_BUCKET_ACCESS_KEY': 'myuseraccesskey',
+    'METRICS_UTILITY_BUCKET_ENDPOINT': os.getenv('METRICS_UTILITY_BUCKET_ENDPOINT', 'http://localhost:9000'),  # or http://minio:9000
+    'METRICS_UTILITY_BUCKET_NAME': 'metricsutilitys3',
+    'METRICS_UTILITY_BUCKET_REGION': 'us-east-1',
+    'METRICS_UTILITY_BUCKET_SECRET_KEY': 'myusersecretkey',
+    'METRICS_UTILITY_REPORT_TYPE': 'CCSPv2',
+    'METRICS_UTILITY_SHIP_PATH': 'metrics-utility/shipped_data',
+    'METRICS_UTILITY_SHIP_TARGET': 's3',
+    'AWX_LOGGING_MODE': 'stdout',
+}
+
+
+@pytest.mark.filterwarnings('ignore::ResourceWarning')
+def test_command():
+    """Build xlsx report using build command and test its contents."""
+
+    python_executable = sys.executable
+    result = subprocess.run(
+        [python_executable, 'manage.py', 'gather_automation_controller_billing_data', '--ship', '--until=10m', '--force'],
+        text=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        env=env_vars,
+    )
+
+    assert result.returncode == 0
+
+    # mc ls -r local/metricsutilitys3/metrics-utility/shipped_data/data/
