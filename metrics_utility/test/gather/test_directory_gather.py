9291b561d0b8cf0413fae45088b19e2989fc632a
Martin Hradil <mhradil@redhat.com>
2025-03-06 14:18:53 +0000

    Test gather with directory output
    Issue: AAP-39221

---
+++ metrics_utility/test/gather/test_directory_gather.py
@@ -0,0 +1,39 @@
+import glob
+import pytest
+import subprocess
+import sys
+from datetime import datetime
+
+env_vars = {
+    'METRICS_UTILITY_REPORT_TYPE': 'CCSPv2',
+    'METRICS_UTILITY_SHIP_PATH': './metrics_utility/test/test_data',
+    'METRICS_UTILITY_SHIP_TARGET': 'directory',
+    'AWX_LOGGING_MODE': 'stdout',
+}
+
+year = datetime.now().strftime('%Y')
+uuid = '00000000-0000-0000-0000-000000000000'  # mock_awx INSTALL_UUID setting
+
+file_glob = f'./metrics_utility/test/test_data/data/{year}/*/*/{uuid}-*.tar.gz'
+
+
+def validate_exists(file_glob):
+    assert len(glob.glob(file_glob)) > 0
+
+
+@pytest.mark.filterwarnings('ignore::ResourceWarning')
+def test_command():
+    """Build xlsx report using build command and test its contents."""
+
+    python_executable = sys.executable
+    result = subprocess.run(
+        [python_executable, 'manage.py', 'gather_automation_controller_billing_data', '--ship', '--until=10m', '--force'],
+        text=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        env=env_vars,
+    )
+
+    assert result.returncode == 0
+
+    validate_exists(file_glob)
