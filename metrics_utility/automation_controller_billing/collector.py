053b756e19d0610c87b61f0e5b1a5f5a4b5c96ca
Martin Hradil <mhradil@redhat.com>
2025-03-20 14:21:48 +0000

    ruff check - drop unused import

--- metrics_utility/automation_controller_billing/collector.py
+++ metrics_utility/automation_controller_billing/collector.py
@@ -2,8 +2,6 @@ import contextlib
 import json
 import logging
 
-import importlib.util
-
 import insights_analytics_collector as base
 from django.conf import settings
 from django.core.serializers.json import DjangoJSONEncoder
