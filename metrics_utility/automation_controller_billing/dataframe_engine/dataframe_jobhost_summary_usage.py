acc99b161d84504e5c4d552817f810abb03f9ec6
Martin Hradil <mhradil@redhat.com>
2025-04-28 00:29:41 +0000

    Simplify job_host_summary || indirect_nodes logic

--- metrics_utility/automation_controller_billing/dataframe_engine/dataframe_jobhost_summary_usage.py
+++ metrics_utility/automation_controller_billing/dataframe_engine/dataframe_jobhost_summary_usage.py
@@ -26,21 +26,17 @@ class DataframeJobhostSummaryUsage(Base):
             ###############################
 
             for data in self.extractor.iter_batches(date=date):
-                # If the dataframe is empty, skip additional processing
                 billing_data = data['job_host_summary']
                 managed_node_type = DIRECT
 
-                if not billing_data.empty:
-                    billing_data['managed_node_type'] = managed_node_type
-                else:
+                if billing_data.empty:
                     billing_data = data['indirect_nodes']
                     managed_node_type = INDIRECT
 
-                    if billing_data.empty:
-                        continue
+                if billing_data.empty:
+                    continue
 
-                    billing_data['managed_node_type'] = managed_node_type
-
+                billing_data['managed_node_type'] = managed_node_type
                 billing_data['managed_node_type_string'] = MANAGED_NODE_TYPES[managed_node_type]
 
                 print_debug(f'\nComputing data batch for {date}')
