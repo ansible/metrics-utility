88e034bfaf7885b9abfd61dd8e3547e802c7218a
Martin Hradil <mhradil@redhat.com>
2025-04-29 21:30:09 +0000

    remove base_command
    the actual commands use
    
        from django.core.management.base import BaseCommand

--- metrics_utility/base_command.py
+++
@@ -1,9 +0,0 @@
-import django.core.management.base as base
-
-
-class BaseCommand(base.BaseCommand):
-    def execute(self, *args, **options):
-        output = self.handle(*args, **options)
-        if output:
-            self.stdout.write(output)
-        return output
