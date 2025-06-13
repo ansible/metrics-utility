98a7cfba49ce0cf21e47894ecf924a90b63e966a
MilanPospisil <arkanus@seznam.cz>
2025-06-13 15:42:28 +0200

    SQL for main template

--- tools/docker/main_jobhostsummary.sql
+++ tools/docker/main_jobhostsummary.sql
@@ -1,10 +1,10 @@
 DO $$
 DECLARE
-  default_organization_id   INTEGER;
-  default_inventory_id   INTEGER;
-  default_instance_id  INTEGER;
+  default_organization_id                           INTEGER;
+  default_inventory_id                              INTEGER;
+  default_instance_id                               INTEGER;
   default_instance_uuid UUID := gen_random_uuid();
-  default_unified_job_template_id         INTEGER;
+  default_unified_job_template_id                   INTEGER;
   random_suffix    TEXT := substring(md5(random()::text), 1, 5);
   --
   random_ip        TEXT := 
@@ -217,6 +217,98 @@ BEGIN
                'default_job_template_' || random_suffix,
                default_unified_job_template_id;
   --
+  -- Job Template
+  --
+  INSERT INTO public.main_jobtemplate (
+    unifiedjobtemplate_ptr_id,
+    job_type,
+    playbook,
+    forks,
+    "limit",
+    verbosity,
+    extra_vars,
+    job_tags,
+    force_handlers,
+    skip_tags,
+    start_at_task,
+    become_enabled,
+    host_config_key,
+    ask_variables_on_launch,
+    survey_enabled,
+    survey_spec,
+    inventory_id,
+    ask_limit_on_launch,
+    ask_inventory_on_launch,
+    ask_credential_on_launch,
+    ask_job_type_on_launch,
+    ask_tags_on_launch,
+    allow_simultaneous,
+    ask_skip_tags_on_launch,
+    timeout,
+    use_fact_cache,
+    ask_verbosity_on_launch,
+    ask_diff_mode_on_launch,
+    diff_mode,
+    job_slice_count,
+    ask_scm_branch_on_launch,
+    scm_branch,
+    webhook_key,
+    webhook_service,
+    ask_execution_environment_on_launch,
+    ask_forks_on_launch,
+    ask_instance_groups_on_launch,
+    ask_job_slice_count_on_launch,
+    ask_labels_on_launch,
+    ask_timeout_on_launch,
+    prevent_instance_group_fallback
+  )
+  VALUES (
+    default_unified_job_template_id,  -- the FK you just created
+    'manual',                         -- job_type
+    '',                               -- playbook
+    0,                                -- forks
+    '',                               -- limit
+    0,                                -- verbosity
+    '{}'::text,                       -- extra_vars
+    '{}'::text,                       -- job_tags
+    false,                            -- force_handlers
+    '',                               -- skip_tags
+    '',                               -- start_at_task
+    false,                            -- become_enabled
+    '',                               -- host_config_key
+    false,                            -- ask_variables_on_launch
+    false,                            -- survey_enabled
+    '{}'::jsonb,                      -- survey_spec
+    default_inventory_id,             -- inventory_id
+    false,                            -- ask_limit_on_launch
+    false,                            -- ask_inventory_on_launch
+    false,                            -- ask_credential_on_launch
+    false,                            -- ask_job_type_on_launch
+    false,                            -- ask_tags_on_launch
+    false,                            -- allow_simultaneous
+    false,                            -- ask_skip_tags_on_launch
+    0,                                -- timeout
+    false,                            -- use_fact_cache
+    false,                            -- ask_verbosity_on_launch
+    false,                            -- ask_diff_mode_on_launch
+    false,                            -- diff_mode
+    0,                                -- job_slice_count
+    false,                            -- ask_scm_branch_on_launch
+    '',                               -- scm_branch
+    '',                               -- webhook_key
+    '',                               -- webhook_service
+    false,                            -- ask_execution_environment_on_launch
+    false,                            -- ask_forks_on_launch
+    false,                            -- ask_instance_groups_on_launch
+    false,                            -- ask_job_slice_count_on_launch
+    false,                            -- ask_labels_on_launch
+    false,                            -- ask_timeout_on_launch
+    false                             -- prevent_instance_group_fallback
+  );
+  --
+  RAISE NOTICE 'Inserted Main JobTemplate ptr_id = %',
+               default_unified_job_template_id;
+  --
   -- Unified Jobs
   -- Loop to create unified jobs
   FOR i IN 1..5 LOOP
@@ -237,6 +329,7 @@ BEGIN
       result_traceback,
       celery_task_id,
       unified_job_template_id,
+      organization_id,
       execution_node,
       emitted_events,
       controller_node,
@@ -263,6 +356,7 @@ BEGIN
       ''::text,                               -- result_traceback
       gen_random_uuid()::text,                -- celery_task_id
       default_unified_job_template_id,        -- FK to your template
+      default_organization_id,
       'auto',                                 -- execution_node
       0,                                      -- emitted_events
       'controller1',                          -- controller_node
@@ -277,9 +371,68 @@ BEGIN
 
     -- Append to our array
     unified_jobs := array_append(unified_jobs, unified_job_id);
+    --
+    -- Create Main Job and connect it with unified job using its id
+    --
+    INSERT INTO public.main_job (
+      unifiedjob_ptr_id,
+      job_type,
+      playbook,
+      forks,
+      "limit",
+      verbosity,
+      extra_vars,
+      job_tags,
+      force_handlers,
+      skip_tags,
+      start_at_task,
+      become_enabled,
+      inventory_id,
+      job_template_id,
+      allow_simultaneous,
+      artifacts,
+      timeout,
+      scm_revision,
+      use_fact_cache,
+      diff_mode,
+      job_slice_count,
+      job_slice_number,
+      scm_branch,
+      webhook_guid,
+      webhook_service,
+      survey_passwords
+    )
+    VALUES (
+      unified_job_id,               -- link to the unified job
+      'manual',                     -- job_type
+      '',                           -- playbook
+      0,                            -- forks
+      '',                           -- "limit"
+      0,                            -- verbosity
+      '{}'::text,                   -- extra_vars
+      '{}'::text,                   -- job_tags
+      false,                        -- force_handlers
+      '',                           -- skip_tags
+      '',                           -- start_at_task
+      false,                        -- become_enabled
+      default_inventory_id,         -- from your DECLARE
+      default_unified_job_template_id, -- from your DECLARE
+      false,                        -- allow_simultaneous
+      '{}'::text,                   -- artifacts
+      0,                            -- timeout
+      '',                           -- scm_revision
+      false,                        -- use_fact_cache
+      false,                        -- diff_mode
+      0,                            -- job_slice_count
+      0,                            -- job_slice_number
+      '',                           -- scm_branch
+      gen_random_uuid()::text,      -- webhook_guid
+      'github',                     -- webhook_service
+      '{}'::jsonb                   -- survey_passwords
+    );
   END LOOP;
   --
-  RAISE NOTICE 'Inserted % unified jobs with IDs: %',
+  RAISE NOTICE 'Inserted % unified jobs and jobs with IDs: %',
                array_length(unified_jobs,1),
                unified_jobs;
   --
