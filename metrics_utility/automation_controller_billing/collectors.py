7064bfa10ce6de12d17021a83c67863ded98e889
MilanPospisil <arkanus@seznam.cz>
2025-06-18 17:10:17 +0200

    Optimize job host summary gather

--- metrics_utility/automation_controller_billing/collectors.py
+++ metrics_utility/automation_controller_billing/collectors.py
@@ -236,15 +236,19 @@ def job_host_summary_table(since, full_path, until, **kwargs):
         $$
         LANGUAGE plpgsql;
     """
-
     query = f"""
-        (SELECT main_jobhostsummary.id,
-                main_jobhostsummary.created,
-                main_jobhostsummary.modified,
-                main_jobhostsummary.host_name,
-                main_jobhostsummary.host_id as host_remote_id,
-                CASE
-                    WHEN (metrics_utility_is_valid_json(main_host.variables))
+    WITH
+    filtered_hosts AS (
+        SELECT DISTINCT main_jobhostsummary.host_id
+        FROM main_jobhostsummary
+        WHERE (main_jobhostsummary.modified >= '{since.isoformat()}'
+        AND main_jobhostsummary.modified < '{until.isoformat()}')
+    ),
+    hosts_variables as (
+        SELECT
+            filtered_hosts.host_id,
+            CASE
+                WHEN (metrics_utility_is_valid_json(main_host.variables))
                         THEN main_host.variables::jsonb->>'ansible_host'
                     ELSE metrics_utility_parse_yaml_field(main_host.variables, 'ansible_host' )
                 END AS ansible_host_variable,
@@ -252,45 +256,56 @@ def job_host_summary_table(since, full_path, until, **kwargs):
                     WHEN (metrics_utility_is_valid_json(main_host.variables))
                         THEN main_host.variables::jsonb->>'ansible_connection'
                     ELSE metrics_utility_parse_yaml_field(main_host.variables, 'ansible_connection' )
-                END AS ansible_connection_variable,
-                -- main_jobhostsummary.constructed_host_id,
-                main_jobhostsummary.changed,
-                main_jobhostsummary.dark,
-                main_jobhostsummary.failures,
-                main_jobhostsummary.ok,
-                main_jobhostsummary.processed,
-                main_jobhostsummary.skipped,
-                main_jobhostsummary.failed,
-                main_jobhostsummary.ignored,
-                main_jobhostsummary.rescued,
-                main_unifiedjob.created AS job_created,
-                main_jobhostsummary.job_id AS job_remote_id,
-                main_unifiedjob.unified_job_template_id AS job_template_remote_id,
-                main_unifiedjob.name AS job_template_name,
-                main_inventory.id AS inventory_remote_id,
-                main_inventory.name AS inventory_name,
-                main_organization.id AS organization_remote_id,
-                main_organization.name AS organization_name,
-                main_unifiedjobtemplate_project.id AS project_remote_id,
-                main_unifiedjobtemplate_project.name AS project_name
-                FROM main_jobhostsummary
-                -- connect to main_job, that has connections into inventory and project
-                LEFT JOIN main_job ON main_jobhostsummary.job_id = main_job.unifiedjob_ptr_id
-                -- get project name from project_options
-                LEFT JOIN main_unifiedjobtemplate AS main_unifiedjobtemplate_project ON main_unifiedjobtemplate_project.id = main_job.project_id
-                -- get inventory name from main_inventory
-                LEFT JOIN main_inventory ON main_inventory.id = main_job.inventory_id
-                -- get job name from main_unifiedjob
-                LEFT JOIN main_unifiedjob ON main_unifiedjob.id = main_jobhostsummary.job_id
-                -- get organization name from main_organization
-                LEFT JOIN main_organization ON main_organization.id = main_unifiedjob.organization_id
-                -- get variables from main_host
-                LEFT JOIN main_host ON main_host.id = main_jobhostsummary.host_id
-                WHERE (main_jobhostsummary.modified >= '{since.isoformat()}' AND main_jobhostsummary.modified < '{until.isoformat()}')
-                ORDER BY main_jobhostsummary.modified ASC)
-        """
+                END AS ansible_connection_variable
 
-    return _copy_table(table='main_jobhostsummary', query=f'COPY {query} TO STDOUT WITH CSV HEADER', path=full_path, prepend_query=prepend_query)
+        FROM filtered_hosts
+        LEFT JOIN main_host ON main_host.id = filtered_hosts.host_id)
+
+        SELECT main_jobhostsummary.id,
+            main_jobhostsummary.created,
+            main_jobhostsummary.modified,
+            main_jobhostsummary.host_name,
+            main_jobhostsummary.host_id as host_remote_id,
+            hosts_variables.ansible_host_variable,
+            hosts_variables.ansible_connection_variable,
+            main_jobhostsummary.changed,
+            main_jobhostsummary.dark,
+            main_jobhostsummary.failures,
+            main_jobhostsummary.ok,
+            main_jobhostsummary.processed,
+            main_jobhostsummary.skipped,
+            main_jobhostsummary.failed,
+            main_jobhostsummary.ignored,
+            main_jobhostsummary.rescued,
+            main_unifiedjob.created AS job_created,
+            main_jobhostsummary.job_id AS job_remote_id,
+            main_unifiedjob.unified_job_template_id AS job_template_remote_id,
+            main_unifiedjob.name AS job_template_name,
+            main_inventory.id AS inventory_remote_id,
+            main_inventory.name AS inventory_name,
+            main_organization.id AS organization_remote_id,
+            main_organization.name AS organization_name,
+            main_unifiedjobtemplate_project.id AS project_remote_id,
+            main_unifiedjobtemplate_project.name AS project_name
+            FROM main_jobhostsummary
+            -- connect to main_job, that has connections into inventory and project
+            LEFT JOIN main_job ON main_jobhostsummary.job_id = main_job.unifiedjob_ptr_id
+            -- get project name from project_options
+            LEFT JOIN main_unifiedjobtemplate AS main_unifiedjobtemplate_project ON main_unifiedjobtemplate_project.id = main_job.project_id
+            -- get inventory name from main_inventory
+            LEFT JOIN main_inventory ON main_inventory.id = main_job.inventory_id
+            -- get job name from main_unifiedjob
+            LEFT JOIN main_unifiedjob ON main_unifiedjob.id = main_jobhostsummary.job_id
+            -- get organization name from main_organization
+            LEFT JOIN main_organization ON main_organization.id = main_unifiedjob.organization_id
+            -- get variables from precomputed hosts_variables
+            LEFT JOIN hosts_variables ON hosts_variables.host_id = main_jobhostsummary.host_id
+            WHERE (main_jobhostsummary.modified >= '{since.isoformat()}'
+            AND main_jobhostsummary.modified < '{until.isoformat()}')
+            ORDER BY main_jobhostsummary.modified ASC
+    """
+
+    return _copy_table(table='main_jobhostsummary', query=f'COPY ({query}) TO STDOUT WITH CSV HEADER', path=full_path, prepend_query=prepend_query)
 
 
 @register('main_jobevent', '1.0', format='csv', description=_('Content usage'), fnc_slicing=daily_slicing)
