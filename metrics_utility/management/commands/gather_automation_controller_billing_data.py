f3f8444c7113ed5619f3a6657a51fdab67d369d6
MilanPospisil <arkanus@seznam.cz>
2025-07-16 16:45:16 +0200

    Remove uneccesary code

--- metrics_utility/management/commands/gather_automation_controller_billing_data.py
+++ metrics_utility/management/commands/gather_automation_controller_billing_data.py
@@ -73,12 +73,7 @@ class Command(BaseCommand):
         )
 
         tgzfiles = collector.gather(since=since, until=until, billing_provider_params=billing_provider_params)
-        if tgzfiles:
-            #self.logger.info(f"Created {len(tgzfiles)} tarball(s)")
-            for tgz in tgzfiles:
-                # self.logger.info(tgz)  # Suppressed to reduce log spam
-                pass
-        else:
+        if not tgzfiles:
             self.logger.error('No analytics collected')
             raise NoAnalyticsCollected('No analytics collected')
 
