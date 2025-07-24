479c401cbdf11f0d4e546a2ab24d139f0530f1a6
itdove <dvernier@redhat.com>
2025-07-24 10:30:35 -0400

    Format

--- metrics_utility/logger.py
+++ metrics_utility/logger.py
@@ -7,9 +7,8 @@ logger = logging.getLogger(__name__)
 
 # This logger will log all message info and up
 logger_info_level = logging.getLogger(__name__)
-logger_info_level.setLevel(logger.INFO)
+logger_info_level.setLevel(logging.INFO)
 
 
 def debug():
     logger.setLevel(logging.DEBUG)
-
