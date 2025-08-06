c3e48bf26dfdff8e7755eaf916b8dfe714bbf51a
MilanPospisil <arkanus@seznam.cz>
2025-08-06 13:28:54 +0200

    Disable logging disabled collectors

--- metrics_utility/base/collector.py
+++ metrics_utility/base/collector.py
@@ -11,14 +11,13 @@ from abc import abstractmethod
 
 from django.utils.timezone import now, timedelta
 
-from metrics_utility.automation_controller_billing.collectors import get_optional_collectors
 from metrics_utility.logger import logger
 
 from .collection import Collection
 from .collection_csv import CollectionCSV
 from .collection_json import CollectionJSON
 from .package import Package
-from .utils import get_max_gather_period_days
+from .utils import get_max_gather_period_days, get_optional_collectors
 
 
 class Collector:
@@ -293,18 +292,23 @@ class Collector:
         disable_job_host_summary_str = os.environ.get('METRICS_UTILITY_DISABLE_JOB_HOST_SUMMARY_COLLECTOR', 'false')
         disable_job_host_summary = disable_job_host_summary_str.lower() == 'true'
 
+        optional_collectors = get_optional_collectors()
+
         for collection in self.collections[Collection.COLLECTION_TYPE_CSV]:
             if last_key != collection.key:
                 write_enabled = False
 
-                if collection.key != 'job_host_summary' or not disable_job_host_summary:
+                if collection.key == 'job_host_summary' and not disable_job_host_summary:
                     write_enabled = True
 
-                if collection.key in get_optional_collectors():
+                if collection.key in optional_collectors:
                     write_enabled = True
 
                 if write_enabled:
                     logger.warning(f'Progress info: Now gathering {collection.key}')
+                else:
+                    logger.warning(f'Progress info: Skipping {collection.key} because it is not enabled.')
+
                 last_key = collection.key
 
             collection.gather(self._package_class().max_data_size())
