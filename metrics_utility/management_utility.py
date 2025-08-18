61ddbe1618729261b0d7fe02320e8e28cb762c4d
MilanPospisil <arkanus@seznam.cz>
2025-08-18 16:52:32 +0200

    Fix help text

--- metrics_utility/management_utility.py
+++ metrics_utility/management_utility.py
@@ -57,6 +57,13 @@ class ManagementUtility(management.ManagementUtility):
         else:
             self.run_subcommand(subcommand, self.argv)
 
+    def main_help_text(self, commands_only=False):
+        commands = 'Commands: build_report, gather_automation_controller_billing_data'
+        if commands_only:
+            return commands
+        else:
+            return f'Usage: manage.py <command> [options]\n{commands}'
+
     def fetch_command(self, subcommand):
         try:
             module = import_module(f'metrics_utility.management.commands.{subcommand}')
