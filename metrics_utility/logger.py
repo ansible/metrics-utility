f522e5be9d524e0832401a93ffa6af9dbc3b2b76
MilanPospisil <arkanus@seznam.cz>
2025-08-19 15:15:59 +0200

    Turn of correct warnings

--- metrics_utility/logger.py
+++ metrics_utility/logger.py
@@ -1,16 +1,11 @@
 import logging
 import sys
+import warnings
 
 
 VERBOSE = '--verbose' in sys.argv
-
-
-class NoWarningsFilter(logging.Filter):
-    def filter(self, record):
-        if VERBOSE:  # allow everything in verbose mode
-            return True
-        return record.levelno != logging.WARNING
-
+if not VERBOSE:
+    warnings.simplefilter(action='ignore', category=FutureWarning)
 
 # FIXME: warning
 logging.basicConfig(format='%(message)s', level=logging.INFO)
@@ -19,7 +14,6 @@ logger = logging.getLogger(__name__)
 # This logger will log all message info and up
 logger_info_level = logging.getLogger(__name__)
 logger_info_level.setLevel(logging.INFO)
-logger.addFilter(NoWarningsFilter())
 
 
 def debug():
