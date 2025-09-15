2cb53f41d2ac778a6ba6151b002cb5961c099d4e
MilanPospisil <arkanus@seznam.cz>
2025-09-15 11:33:50 +0200

    Collect all the events from analytics and metrics combined

--- metrics_utility/automation_controller_billing/collectors.py
+++ metrics_utility/automation_controller_billing/collectors.py
@@ -818,3 +818,76 @@ def job_host_summary_table(since, full_path, until, **kwargs):
         path=full_path,
         prepend_query=prepend_query
     )
+
+@register('main_jobevent_service', '1.1', format='csv', description=_('Content usage'), fnc_slicing=daily_slicing)
+def main_jobevent_table(since, full_path, until, **kwargs):
+    event_data = rf"replace(main_jobevent.event_data, '\u', '\u005cu')::jsonb"
+
+    query = f"""
+    WITH job_scope AS (
+        SELECT
+            main_jobhostsummary.id AS main_jobhostsummary_id,
+            main_jobhostsummary.created AS main_jobhostsummary_created,
+            main_jobhostsummary.modified AS main_jobhostsummary_modified,
+            main_unifiedjob.created AS job_created,
+            main_jobhostsummary.job_id AS job_id,
+            main_jobhostsummary.host_name
+        FROM main_jobhostsummary
+        JOIN main_unifiedjob
+          ON main_unifiedjob.id = main_jobhostsummary.job_id
+        WHERE (
+            main_jobhostsummary.modified >= '{since.isoformat()}'
+            AND main_jobhostsummary.modified < '{until.isoformat()}'
+        )
+    )
+    SELECT
+        job_scope.main_jobhostsummary_id,
+        job_scope.main_jobhostsummary_created,
+        job_scope.main_jobhostsummary_modified,
+
+        main_jobevent.id,
+        main_jobevent.created,
+        main_jobevent.modified,
+        main_jobevent.job_created,
+        main_jobevent.uuid,
+        main_jobevent.parent_uuid,
+        main_jobevent.event,
+
+        -- JSON extracted fields
+        ({event_data}->>'task_action')::TEXT AS task_action,
+        ({event_data}->>'resolved_action')::TEXT AS resolved_action,
+        ({event_data}->>'resolved_role')::TEXT AS resolved_role,
+        ({event_data}->>'duration')::TEXT AS duration,
+        ({event_data}->>'start')::timestamptz AS start,
+        ({event_data}->>'end')::timestamptz AS end,
+
+        CASE WHEN main_jobevent.event = 'playbook_on_stats'
+             THEN {event_data} - 'artifact_data'
+        END AS playbook_on_stats,
+
+        main_jobevent.failed,
+        main_jobevent.changed,
+        main_jobevent.playbook,
+        main_jobevent.play,
+        main_jobevent.task,
+        main_jobevent.role,
+        main_jobevent.job_id AS job_remote_id,
+        main_jobevent.host_id AS host_remote_id,
+        main_jobevent.host_name,
+
+        -- Warnings and deprecations (json arrays)
+        {event_data}->'res'->'warnings' AS warnings,
+        {event_data}->'res'->'deprecations' AS deprecations
+
+    FROM main_jobevent
+    JOIN job_scope
+      ON job_scope.job_created = main_jobevent.job_created
+     AND job_scope.job_id = main_jobevent.job_id
+     AND job_scope.host_name = main_jobevent.host_name
+    """  # dropped WHERE {tbl}.event IN (...) because we want to collect all events
+    
+    return _copy_table(
+        table='main_jobevent',
+        query=f'COPY ({query}) TO STDOUT WITH CSV HEADER',
+        path=full_path
+    )
