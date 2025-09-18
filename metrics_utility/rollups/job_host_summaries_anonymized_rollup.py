b5750a8605e2f8434372ce2a05dd6838cd55e1b3
MilanPospisil <arkanus@seznam.cz>
2025-09-18 13:57:30 +0200

    EE anonymized2

---
+++ metrics_utility/rollups/job_host_summaries_anonymized_rollup.py
@@ -0,0 +1,14 @@
+class Job_Host_Summaries_Anonymized_Rollup:
+    """
+    Collector - job_host_summaries_service collector data
+    """
+
+    def base(dataframe):
+        """
+        Avg tasks by template 
+        Number of tasks executed
+        Success ratio of tasks exectuted 
+        """
+        
+        
+        
