9265ab9ee4463ceca0bd357d893a0ff2cc9c3e12
MilanPospisil <arkanus@seznam.cz>
2025-09-30 14:15:36 +0200

    Events metrics - module oriented 2

--- metrics_utility/anonymized_rollups/events_anonymized_rollups.py
+++ metrics_utility/anonymized_rollups/events_anonymized_rollups.py
@@ -25,6 +25,18 @@ class Event_Anonymized_Rollups:
     @staticmethod
     def prepare_data(dataframe):
          # Prepare data
+
+        dataframe = dataframe[
+            dataframe['module_name'].notna()
+            & dataframe['host_id'].notna()
+            & dataframe['playbook'].notna()
+            & dataframe['job_id'].notna()
+            & (dataframe['module_name'].str.strip() != '')
+            & (dataframe['host_id'].str.strip() != '')
+            & (dataframe['playbook'].str.strip() != '')
+            & (dataframe['job_id'].str.strip() != '')
+        ]
+
         # add module column into the dataframe based on dataframe_content_usage.py approach
         dataframe['task_action'] = dataframe.resolved_action.fillna(dataframe.task_action).astype(str)
         dataframe.rename(columns={'task_action': 'module_name'}, inplace=True)
@@ -41,6 +53,8 @@ class Event_Anonymized_Rollups:
         # for task success and failure
         dataframe['task_success_event'] = dataframe['task_success_event'].fillna(False).astype(bool)
         dataframe['task_failed_event'] = dataframe['task_failed_event'].fillna(False).astype(bool)
+
+        return dataframe
 
     @staticmethod
     def event_collections_aggregations(dataframe):
@@ -89,7 +103,7 @@ class Event_Anonymized_Rollups:
         }
 
     @staticmethod
-    def events_aggregations(dataframe):
+    def events_modules_aggregations(dataframe):
         '''
         ?Number of jobs executed that use a specific partner collection.
         *Avg number of modules used in a playbook
@@ -100,7 +114,6 @@ class Event_Anonymized_Rollups:
 
         # ?Number of jobs executed that use a specific partner collection.
 
-        
         # Modules used to automate
         # distinct name of modules used to automate
         list_of_modules_used_to_automate = dataframe['module_name'].unique().tolist()
@@ -139,6 +152,7 @@ class Event_Anonymized_Rollups:
         )
 
         # Per-module counts
+        # receiver of this data can easily calculate success rates
         module_stats = (
             task_summary.groupby('module_name')
             .agg(
@@ -154,126 +168,10 @@ class Event_Anonymized_Rollups:
         )
     
 
-
-
-
-
-
-
-
-
-
-
-
-    # TODO - will use rollup data from events rollups - need to update events rollups
     @staticmethod
     def base(dataframe):
-        """
-        This function will create first level aggregation of the event dataframe, the result is json
-
-        Aggregations are:
-        
-            ?Number of jobs executed that use a specific partner collection.
-            - Total number of jobs executed by collection source
-            - Avg number of modules used in a playbook
-            - Failure/Success rate of modules
-            - Modules Used to Automate
-            - Total number of modules automated
-        """
-
-        Event_Anonymized_Rollups.prepare_data(dataframe)
+       
+        dataframe = Event_Anonymized_Rollups.prepare_data(dataframe)
 
         event_collections_aggregations = Event_Anonymized_Rollups.event_collections_aggregations(dataframe)
         events_aggregations = Event_Anonymized_Rollups.events_aggregations(dataframe)
-
-
-        '''
-        # group by playbook, module, include job_failed column
-        aggregations_by_playbook_module = (
-            dataframe.groupby(['playbook', 'module_name'])
-            .agg(
-                job_failed=('job_failed', 'max'),
-                job_total=('job_id', 'nunique'),
-            )
-            .reset_index()  # <-- make keys real columns
-        )
-
-        # modules used to automate
-        list_of_modules_used_to_automate = aggregations_by_playbook_module['module_name'].unique().tolist()
-        total_modules_used_to_automate = len(list_of_modules_used_to_automate)
-
-        # return as object that can be converted to json
-        return {
-            'aggregations_by_playbook_module': aggregations_by_playbook_module.to_dict(orient='records'),
-            'list_of_modules_used_to_automate': list_of_modules_used_to_automate,
-            'total_modules_used_to_automate': total_modules_used_to_automate,
-        }
-        '''
-
-
-class Events_Collections_Anonymized_Rollups:
-    """
-    Collectors -
-    - unified_jobs
-    - main_jobevent_service
-    """
-
-    # TODO - will use rollup data from events rollups - need to update events rollups
-    @staticmethod
-    def base(dataframe):
-        """
-        events, jobs - dataframes from collectors
-        This function will create first level aggregation of the job dataframe, the result is json
-
-        *Breakdown of total jobs executed by collection source (e.g., Red Hat, Partner A, Community).
-        *Average job duration for collection sources
-        *Average number of hosts automated per job for each collection source.
-        *Number of jobs per collection source that have failed.
-        *Success/failure rate of jobs per collection source.
-
-        ?Number of jobs executed that use a specific partner collection.
-        """
-        '''
-        events = dataframe
-
-        events = events.assign(job_failed=events['job_failed'].fillna(False).astype(bool))
-        events['collection_name'] = events['module_name'].apply(extract_collection_name)
-
-        events['job_duration'] = events['job_finished'] - events['job_started']
-        events['job_waiting_time'] = events['job_started'] - events['job_created']
-
-        # fill collection source from collections_types
-        events['collection_source'] = events['collection_name'].map(collections_types)
-
-        # Total jobs executed by collection source
-        total_jobs_by_collection_source = events.groupby('collection_source')['job_id'].nunique()
-
-        # Average job duration by collection source
-        # drop duplicates so each (job_id, collection_source) pair has one duration
-        dropped_duplicates = events.drop_duplicates(subset=['job_id', 'collection_source'])
-        avg_job_duration_by_collection_source = dropped_duplicates.groupby('collection_source')['job_duration'].mean()
-
-        # Success/failure rate of jobs per collection source.
-        # column job_failed
-        failed_jobs_by_collection_source = (
-            events[events['job_failed']].groupby('collection_source')['job_id'].nunique().reindex(total_jobs_by_collection_source.index, fill_value=0)
-        )
-        success_rate_by_collection_source = (total_jobs_by_collection_source - failed_jobs_by_collection_source) / total_jobs_by_collection_source
-
-        # Average number of hosts automated per job for each collection source.
-        # split data by collection source, count the number of unique host_id per job_id
-        # then average the number of hosts per job for each collection source
-        avg_hosts_per_job_by_collection_source = (
-            events.groupby(['collection_source', 'job_id'])['host_id'].nunique().groupby('collection_source').mean()
-        )
-
-        result = {
-            'total_jobs_by_collection_source': total_jobs_by_collection_source,
-            'avg_job_duration_by_collection_source': avg_job_duration_by_collection_source,
-            'failed_jobs_by_collection_source': failed_jobs_by_collection_source,
-            'success_rate_by_collection_source': success_rate_by_collection_source,
-            'avg_hosts_per_job_by_collection_source': avg_hosts_per_job_by_collection_source,
-        }
-
-        return result
-        '''
