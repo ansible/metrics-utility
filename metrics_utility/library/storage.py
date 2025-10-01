f89d93dd358aa3cb27aafb4d9a99ff4965583d00
Martin Hradil <mhradil@redhat.com>
2025-10-01 17:30:38 +0000

    claude: library/storage - add crc & dir
    update metrics_utility/library/storage.py to add storagecrc & storagedirectory

--- metrics_utility/library/storage.py
+++ metrics_utility/library/storage.py
@@ -34,3 +34,38 @@ class StorageS3:
     def remove(self, files):
         print("library.storage StorageS3.remove")
         return True
+
+
+class StorageCRC:
+    def __init__(self, settings):
+        print("library.storage StorageCRC.__init__")
+
+    def ship(self, data):
+        print("library.storage StorageCRC.ship")
+        return True
+
+
+class StorageDirectory:
+    def __init__(self, settings):
+        print("library.storage StorageDirectory.__init__")
+
+    def ship(self, data):
+        print("library.storage StorageDirectory.ship")
+        return True
+
+    def glob(self, glob, since=None, until=None):
+        print("library.storage StorageDirectory.glob")
+        return [f"fake-file-{i}.tar.gz" for i in range(3)]
+
+    @contextmanager
+    def get(self, remote):
+        print("library.storage StorageDirectory.get")
+        yield f"/tmp/local-{remote}"
+
+    def put(self, path=None, data=None, file=None):
+        print("library.storage StorageDirectory.put")
+        return True
+
+    def remove(self, files):
+        print("library.storage StorageDirectory.remove")
+        return True
