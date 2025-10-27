849ffc9ab3035383179e0c01828cc570731093cb
MilanPospisil <arkanus@seznam.cz>
2025-10-27 11:49:28 +0100

    csv into tarballs, handle empty tarball

--- metrics_utility/anonymized_rollups/base_anonymized_rollup.py
+++ metrics_utility/anonymized_rollups/base_anonymized_rollup.py
@@ -24,7 +24,9 @@ class BaseAnonymizedRollup:
     def base(self, dataframe):
         return pd.DataFrame()
 
-    def save_rollup(self, rollup_data: dict, base_path: str, year: int, month: int, day: int, save_csv: bool = False) -> None:
+    def save_rollup(
+        self, rollup_data: dict, base_path: str, year: int, month: int, day: int, save_csv: bool = False, save_parquet: bool = True
+    ) -> None:
         # rollup data is dictionary
         # the dictionary can have those values:
         # scalar, list, pandas.Series, pandas.DataFrame
@@ -34,6 +36,7 @@ class BaseAnonymizedRollup:
         # file will be stored inside base_path/rollups/rollup_name/year/month/day
 
         # save_csv is for testing purposes only - so we can check content of files easily
+        # save_parquet controls whether to save parquet files
 
         # make sure year is 4 digits, month is 2 digits, day is 2 digits
         year = str(year).zfill(4)
@@ -43,49 +46,55 @@ class BaseAnonymizedRollup:
 
         os.makedirs(rollup_path, exist_ok=True)
 
-        # Collect JSON data in memory for tar archive
-        json_files = {}
+        # Collect files in memory for tar archive
+        tar_files = {}
 
         for key, value in rollup_data.items():
             filename = key + '_' + str(year) + '_' + str(month) + '_' + str(day)
 
             if isinstance(value, pd.DataFrame):
                 if save_csv:
-                    value.to_csv(os.path.join(rollup_path, f'{key}.csv'), index=False)
+                    # Save CSV to tarball instead of filesystem
+                    csv_buffer = io.StringIO()
+                    value.to_csv(csv_buffer, index=False)
+                    tar_files[f'{key}.csv'] = csv_buffer.getvalue().encode('utf-8')
 
-                # to parquet
-                value.to_parquet(os.path.join(rollup_path, f'{filename}.parquet'), index=False)
+                # to parquet (outside tarball)
+                if save_parquet:
+                    value.to_parquet(os.path.join(rollup_path, f'{filename}.parquet'), index=False)
+
             elif isinstance(value, pd.Series):
                 # Convert Series to DataFrame to preserve index with proper column names
                 df = value.reset_index()
 
                 if save_csv:
-                    df.to_csv(os.path.join(rollup_path, f'{key}.csv'), index=False)
+                    # Save CSV to tarball instead of filesystem
+                    csv_buffer = io.StringIO()
+                    df.to_csv(csv_buffer, index=False)
+                    tar_files[f'{key}.csv'] = csv_buffer.getvalue().encode('utf-8')
 
-                df.to_parquet(os.path.join(rollup_path, f'{filename}.parquet'), index=False)
+                if save_parquet:
+                    df.to_parquet(os.path.join(rollup_path, f'{filename}.parquet'), index=False)
 
             elif isinstance(value, list):
                 # Store JSON data in memory for tar
-                json_files[f'{filename}.json'] = value
+                tar_files[f'{filename}.json'] = json.dumps(value, indent=2).encode('utf-8')
 
             elif isinstance(value, dict):
                 # Store JSON data in memory for tar
-                json_files[f'{filename}.json'] = value
+                tar_files[f'{filename}.json'] = json.dumps(value, indent=2).encode('utf-8')
             # the rest
             else:
                 print(f'Key {key} is a unknown type')
 
-        # Create tarball from in-memory JSON data (excluding parquet files)
-        # tarball name is data_rollups_<year>_<month>_<day>.tar.gz
-        tar_path = os.path.join(rollup_path, f'data_rollups_{year}_{month}_{day}.tar.gz')
-        with tarfile.open(tar_path, 'w:gz') as tar:
-            for filename, data in json_files.items():
-                # Convert data to JSON bytes
-                json_bytes = json.dumps(data, indent=2).encode('utf-8')
+        # Create tarball only if there are files to add
+        if tar_files:
+            tar_path = os.path.join(rollup_path, f'data_rollups_{year}_{month}_{day}.tar.gz')
+            with tarfile.open(tar_path, 'w:gz') as tar:
+                for filename, data in tar_files.items():
+                    # Create TarInfo object
+                    tarinfo = tarfile.TarInfo(name=f'./{filename}')
+                    tarinfo.size = len(data)
 
-                # Create TarInfo object
-                tarinfo = tarfile.TarInfo(name=f'./{filename}')
-                tarinfo.size = len(json_bytes)
-
-                # Add to tar from memory
-                tar.addfile(tarinfo, io.BytesIO(json_bytes))
+                    # Add to tar from memory
+                    tar.addfile(tarinfo, io.BytesIO(data))
