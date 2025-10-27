5521126acd193cf0b1ab552ef3d7ace630f8c6ec
MilanPospisil <arkanus@seznam.cz>
2025-10-27 10:09:31 +0100

    Better cleanup

--- metrics_utility/test/test_anonymized_rollups/test_from_gather_to_json.py
+++ metrics_utility/test/test_anonymized_rollups/test_from_gather_to_json.py
@@ -6,26 +6,25 @@ import pytest
 from metrics_utility.anonymized_rollups.anonymized_rollups import compute_anonymized_rollup_from_raw_data
 from metrics_utility.anonymized_rollups.task_anonymized_rollups import task_anonymized_rollups
 import json
+import shutil
 
 # where to find the tar.gz (match jobhostsummary test layout)
-uuid = '00000000-0000-0000-0000-000000000000'
-file_glob = f'./out/**/{uuid}-*.tar.gz'
-file_paths = f'./out/data/2025/06/13/{uuid}-*.tar.gz'
 
-@pytest.fixture(autouse=True)
+@pytest.fixture
 def cleanup_glob():
-    for file in glob.glob(file_glob, recursive=True):
-        os.remove(file)
-    yield
-    for file in glob.glob(file_glob, recursive=True):
-        os.remove(file)
+    out_dir = "./out"
 
+    # --- Cleanup before test ---
+    if os.path.exists(out_dir):
+        shutil.rmtree(out_dir)
 
+    yield  # Run your test
 
-
+    # --- Cleanup after test ---
+    if os.path.exists(out_dir):
+        shutil.rmtree(out_dir)
 
 def test_empty_data(cleanup_glob):
-    return
     compute_anonymized_rollup_from_raw_data('salt', 2025, 6, 13, True)
 
 
