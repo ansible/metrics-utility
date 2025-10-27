c0f331febc0e7bd518fb579be0c970119260c747
Martin Hradil <mhradil@redhat.com>
2025-10-27 21:54:51 +0000

    Add conf_setting table data to mock db
    Makes sure we have data in the conf_test table for testing,
    with the license info matching the mock `get_license`
    implementation in `mock_awx/awx/conf/license/__init__.py`.

--- tools/docker/conf_setting.sql
+++ tools/docker/conf_setting.sql
@@ -6,9 +6,4 @@ INSERT INTO public.conf_setting (created, modified, key, value) VALUES
   (now(), now(), 'INSTALL_UUID', '"00000000-0000-0000-0000-000000000000"'),
   (now(), now(), 'LICENSE', '{"license_type": "UNLICENSED", "product_name": "AWX", "subscription_name": null, "valid_key": false}'),
   (now(), now(), 'TOWER_URL_BASE', '"https://platformhost"'),
-  -- Test version data
-  (now(), now(), 'VERSION', '"24.6.0"'),
-  (now(), now(), 'VERSION', '"4.7.5"'),
-  (now(), now(), 'VERSION', '"4.7.5"'),
-  -- Test analytics data
   (now(), now(), 'AUTOMATION_ANALYTICS_LAST_ENTRIES', '{"config": "2024-01-01T10:00:00Z", "jobs": "2024-01-02T15:30:00Z"}');
