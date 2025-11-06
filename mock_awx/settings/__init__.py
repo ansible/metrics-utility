e9410258e32bce72648581d20b1be57dca109ceb
Martin Hradil <mhradil@redhat.com>
2025-11-06 18:09:08 +0000

    remove unused hook & settings

--- mock_awx/settings/__init__.py
+++ mock_awx/settings/__init__.py
@@ -19,13 +19,6 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
-# Collected
-AUTOMATION_ANALYTICS_LAST_ENTRIES = ''
+# collected but also used by Package
 INSTALL_UUID = '00000000-0000-0000-0000-000000000000'
-LOG_AGGREGATOR_ENABLED = False
-LOG_AGGREGATOR_LOGGERS = ['awx', 'activity_stream', 'job_events', 'system_tracking', 'broadcast_websocket', 'job_lifecycle']
-LOG_AGGREGATOR_TYPE = None
-PENDO_TRACKING_STATE = 'off'
-SUBSCRIPTION_USAGE_MODEL = ''
 SYSTEM_UUID = '00000000-0000-0000-0000-000000000000'
-TOWER_URL_BASE = 'https://platformhost'
