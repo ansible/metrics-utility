ec608142b19c0f3c8f27c85f4d80458b9929cf16
MilanPospisil <arkanus@seznam.cz>
2025-11-12 12:17:54 +0100

    Delete comments

--- metrics_utility/anonymized_rollups/task_anonymized_rollups.py
+++ metrics_utility/anonymized_rollups/task_anonymized_rollups.py
@@ -7,15 +7,6 @@ from metrics_utility.library.collectors.controller import execution_environments
 
 
 def task_anonymized_rollups(db, salt, year, month, day, ship_path, save_rollups: bool = True):
-    """
-    env_vars = {
-        'METRICS_UTILITY_SHIP_PATH': ship_path,
-        'METRICS_UTILITY_SHIP_TARGET': 'directory',
-        'METRICS_UTILITY_OPTIONAL_COLLECTORS': 'main_jobevent_service,execution_environments,unified_jobs,job_host_summary_service',
-        'METRICS_UTILITY_DISABLE_JOB_HOST_SUMMARY_COLLECTOR': 'true',
-    }
-    """
-
     datetime_since = datetime(year, month, day)
     datetime_until = datetime_since + timedelta(days=1)
 
@@ -31,13 +22,6 @@ def task_anonymized_rollups(db, salt, year, month, day, ship_path, save_rollups:
         'job_host_summary': job_host_summary_data,
         'main_jobevent': main_jobevent_data,
     }
-
-    # since_param = datetime_since.strftime('%Y-%m-%d')
-    # until_param = datetime_until.strftime('%Y-%m-%d')
-
-    # run_gather_int(env_vars, {'ship': True, 'force': True, 'since': since_param, 'until': until_param})
-
-    # run gather from collectors in library
 
     # load data for each collector
     json_data = compute_anonymized_rollup_from_raw_data(input_data, salt, year, month, day, ship_path, save_rollups)
