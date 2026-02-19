fbd102b824c07c657a62ceb891497bd8b24bb6ea
MilanPospisil <arkanus@seznam.cz>
2026-02-19 09:51:41 +0100

    Gather only managed credentials

--- metrics_utility/library/collectors/controller/credentials_service.py
+++ metrics_utility/library/collectors/controller/credentials_service.py
@@ -16,6 +16,7 @@ def credentials_service(*, db=None, since=None, until=None):
         WHERE
             main_unifiedjob.finished >= '{since.isoformat()}'
             AND main_unifiedjob.finished < '{until.isoformat()}'
+            AND main_credentialtype.managed = true
         ORDER BY main_unifiedjob.id ASC, main_credentialtype.name ASC
     """
 
