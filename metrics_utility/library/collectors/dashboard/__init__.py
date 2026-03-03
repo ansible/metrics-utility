4ac87a86f60dfb6d4bcc7e2f0a5d4471aa593b8e
Kristjan Cagran <kristijan.cagran@xlab.si>
2026-03-03 14:30:25 +0100

    Add dashboard collectors
    Signed-off-by: Kristjan Cagran <kristijan.cagran@xlab.si>

---
+++ metrics_utility/library/collectors/dashboard/__init__.py
@@ -0,0 +1,4 @@
+from .collectors import AWXJobHostSummaryType, AWXJobType, DashboardJobsResultType, dashboard_jobs
+
+
+__all__ = ['dashboard_jobs', 'DashboardJobsResultType', 'AWXJobHostSummaryType', 'AWXJobType']
