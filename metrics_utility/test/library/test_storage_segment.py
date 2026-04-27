c96fbbfa11bfa8679a73d7c727b4974dc098d804
MilanPospisil <arkanus@seznam.cz>
2026-04-27 09:52:33 +0200

    Remove use bulk from split into segment (#378)

--- metrics_utility/test/library/test_storage_segment.py
+++ metrics_utility/test/library/test_storage_segment.py
@@ -43,17 +43,6 @@ class TestStorageSegmentAvailable:
         assert len(chunks[2]['module_stats']) == 38
         assert len(chunks[3]['module_stats']) == 36
 
-    def test_correct_splitting_for_large_data_with_bulk(self):
-        storage_segment = StorageSegment(use_bulk=True)
-        chunks = storage_segment._split_into_chunks(segment_data_large, storage_segment.BULK_MESSAGE_LIMIT)
-        # Even with bulk mode, we split by top-level keys
-        assert len(chunks) == 5
-        assert 'statistics' in chunks[0]
-        assert 'module_stats' in chunks[1]
-        assert 'collection_stats' in chunks[2]
-        assert 'jobs_by_job_type' in chunks[3]
-        assert 'job_host_summary' in chunks[4]
-
     def test_simple_list_data(self):
         data = {'test_list': ['item1', 'item2']}
         storage_segment = StorageSegment()
@@ -152,39 +141,3 @@ class TestStorageSegmentAvailable:
             chunk_info = call_kwargs['properties']['chunk_info']
             assert chunk_info['chunk_number'] == i
             assert chunk_info['total_chunks'] == 7
-
-    @patch('metrics_utility.library.storage.segment.analytics')
-    @patch('metrics_utility.library.storage.segment.SEGMENT_AVAILABLE', True)
-    def test_put_sends_single_chunk_for_large_data_with_bulk(self, mock_analytics):
-        """Test that put method with use_bulk=True sends large data in a single chunk."""
-        # Setup
-        mock_analytics.track = Mock()
-        mock_analytics.flush = Mock()
-
-        storage_segment = StorageSegment(write_key='test_write_key', user_id='test_user', debug=True, use_bulk=True)
-
-        # Act
-        chunks = storage_segment.put(artifact_name='test_bulk_artifact', dict=segment_data_large, event_name='Test Bulk Event')
-
-        # Assert
-        # Even with bulk mode, we split by top-level keys (5 chunks)
-        assert len(chunks) == 5
-        assert mock_analytics.track.call_count == 5
-        assert mock_analytics.flush.call_count == 1
-
-        # Verify chunk numbering in the calls
-        for i, call in enumerate(mock_analytics.track.call_args_list, 1):
-            call_kwargs = call[1]
-            chunk_info = call_kwargs['properties']['chunk_info']
-            assert chunk_info['chunk_number'] == i
-            assert chunk_info['total_chunks'] == 5
-
-        # Verify the first call
-        call_args = mock_analytics.track.call_args_list[0][1]
-        assert call_args['event'] == 'Test Bulk Event'
-        assert call_args['properties']['artifact_name'] == 'test_bulk_artifact'
-
-        # Verify each chunk contains one top-level key
-        for i, call in enumerate(mock_analytics.track.call_args_list):
-            data = call[1]['properties']['data']
-            assert len(data) == 1, f'Chunk {i + 1} should contain exactly one top-level key'
