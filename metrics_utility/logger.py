420b03cf915deed7a5de5e024a8d9127bbf9fc3d
Martin Hradil <mhradil@redhat.com>
2026-05-22 23:26:07 +0000

    drop the FutureWarning ignore

--- metrics_utility/logger.py
+++ metrics_utility/logger.py
@@ -1,12 +1,7 @@
 """Configure and expose the metrics-utility logger and debug helper."""
 
 import logging
-import sys
-import warnings
 
-
-if sys.argv[0].endswith('manage.py'):
-    warnings.simplefilter(action='ignore', category=FutureWarning)
 
 # note: total_workers_vcpu requires at least INFO
 logging.basicConfig(format='%(message)s', level=logging.INFO)
